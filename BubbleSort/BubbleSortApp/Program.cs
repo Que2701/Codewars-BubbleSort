6da9b7dd5b87ded4549df3936d93e879d61768c4
tumelo <tumelom@evolvemed.co.za>
2019-10-01 08:13:01 +0200

    bubble sort with two for loops

--- BubbleSort/BubbleSortApp/Program.cs
+++ BubbleSort/BubbleSortApp/Program.cs
@@ -10,6 +10,40 @@ namespace BubbleSortApp
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[] { 5, 1, 4, 2, 8 };
+            Console.WriteLine("Before swap:");
+            foreach (var number in numbers)
+            {
+                Console.Write($" {number} ");
+            }
+            Console.WriteLine($"\nAfter swap:");
+            BubbleSort(numbers);
+            foreach (var number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine("\n\n");
+            Console.Read();
+        }
+
+        static int[] BubbleSort(int[] numbers)
+        {
+            int swapValue = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if(numbers[i] < numbers[j])
+                    {
+                        swapValue = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = swapValue;
+                    }
+                }
+            }
+            
+            return numbers;
         }
     }
 }
